92ea81ba0d956b86977d570bac6872ce1666d74a
19CN026170 <jogiogunpola01@gmail.com>
2024-05-29 16:11:14 +0100

    Your commit message here

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <stdbool.h>
 
 struct UserReg {
     char FirstName[60];
@@ -14,13 +15,13 @@ struct UserReg {
 // Function to register a user
 void registerUser(struct UserReg *user) {
     printf("Enter Your Firstname: ");
-    scanf("%s", user->FirstName);
+    scanf("%59s", user->FirstName);
 
     printf("Enter Your Lastname: ");
-    scanf("%s", user->LastName);
+    scanf("%69s", user->LastName);
 
     printf("Create Your Password: ");
-    scanf("%s", user->Password);
+    scanf("%11s", user->Password);
 
     user->isRegistered = 1;
 
@@ -39,7 +40,7 @@ void registerUser(struct UserReg *user) {
     fprintf(filePointer, "%s\n", user->LastName);
     fprintf(filePointer, "%s\n", user->Password);
     fprintf(filePointer, "%d\n", user->isRegistered);
-    fprintf(filePointer, "%lld\n", user->AccountNumber);
+        fprintf(filePointer, "%lld\n", user->AccountNumber);
     fclose(filePointer);
 
     printf("\nRegistration Details:\n");
@@ -56,7 +57,7 @@ void readUserDetails(struct UserReg *user) {
         printf("Error opening file!\n");
         return;
     }
-    
+
     if (fgets(user->FirstName, sizeof(user->FirstName), filePointer) == NULL ||
         fgets(user->LastName, sizeof(user->LastName), filePointer) == NULL ||
         fgets(user->Password, sizeof(user->Password), filePointer) == NULL ||
@@ -82,19 +83,67 @@ void userLogin(struct UserReg *user) {
 
     if (storedUser.isRegistered) {
         printf("Enter Your Firstname: ");
-        scanf("%s", user->FirstName);
+        scanf("%59s", user->FirstName);
 
         printf("Enter Your Lastname: ");
-        scanf("%s", user->LastName);
+        scanf("%69s", user->LastName);
 
         printf("Enter Your Password: ");
-        scanf("%s", user->Password);
+        scanf("%11s", user->Password);
 
         if (strcmp(user->FirstName, storedUser.FirstName) == 0 &&
             strcmp(user->LastName, storedUser.LastName) == 0 &&
-            strcmp(user->Password, storedUser.Password) == 0) {
+            strcmp(user->Password, storedUser.Password) == 0)
+            {
             printf("Login successful!\n");
-            printf("Your 10-digit Account number: %lld\n", user->AccountNumber);
+            printf("Your 10-digit Account number: %lld\n", storedUser.AccountNumber);
+
+            int correct_pin = 1234;
+            int user_pin;
+            bool isValid = false;
+
+            printf("Good Day %s\n", user->FirstName); // Welcome the user
+            printf("You are welcome to the JOGI Bank.\n");
+            printf("Your default pin is 1234\n");
+
+            do {
+                printf("Enter your bank pin: ");
+                scanf("%d", &user_pin);
+                if (user_pin == correct_pin) {
+                    printf("Pin correct!\n");
+                    isValid = true;
+                } else {
+                    printf("Invalid pin! Enter again.\n");
+                }
+            } while (!isValid);
+
+            printf("Access Granted\n");
+
+            int Balance = 0;
+            int Deposit = 0;
+            int Withdrawal = 0;
+            int Loan = 0;
+
+            printf("How much would you like to deposit? ");
+            scanf("%d", &Deposit);
+            Balance += Deposit;
+
+            printf("How much would you like to withdraw? ");
+            scanf("%d", &Withdrawal);
+
+            if (Withdrawal > Balance) {
+                printf("Insufficient Funds\n");
+            } else {
+                Balance -= Withdrawal;
+                printf("Here is your %d naira cash.\n", Withdrawal);
+            }
+
+            printf("In the spirit of festivity, we are offering a reduced 8 percent interest rate on all loans taken this month.\nHow much would you like to get? ");
+            scanf("%d", &Loan);
+
+            float lopen = Loan + (Loan *(8/ 100.0));
+            printf("Congratulations, all you'll need to do is pay up %.2f over the next 12 months.\n", lopen);
+
         } else {
             printf("Login failed! Incorrect details.\n");
         }
@@ -104,17 +153,20 @@ void userLogin(struct UserReg *user) {
 }
 
 int main() {
-    struct UserReg user = { .isRegistered = 0 };
+    struct UserReg user = { .isRegistered = 0 }; // Initialize user with isRegistered set to 0
     int Numb;
     int ContinueLoop = 1;
 
+    // Main loop to handle user input
     while (ContinueLoop) {
+        // Display menu
         printf("1. Register\n");
         printf("2. Login\n");
         printf("3. Exit\n");
         printf("You are welcome to the JOGI Bank, pick between 1 and 2 to Register or Login: ");
         scanf("%d", &Numb);
 
+        // Handle user choice
         switch (Numb) {
             case 1:
                 registerUser(&user);
